11c90d0eefa58390dd2ae57bbd5fe22edad604cc
dent500@gmail.com <dent500@gmail.com@b32a8489-b907-6ae1-0a5f-8c12d9beddb8>
2012-09-17 17:27:22 +0000

    working on login page

--- trunk/app/Model/User.php
+++ trunk/app/Model/User.php
@@ -16,12 +16,12 @@ App::uses('AuthComponent', 'Controller/Component');
  */
 
 class User extends AppModel {
-   public $name = 'User';
-    public $validates = array(
+    public $name = 'User';
+    public $user_validate = array(
         'user_email' => array(
             'required' => array(
                 'rule' => array('notEmpty'),
-                'message' => 'A user email is required'
+                'message' => 'A username is required'
             )
         ),
         'user_password' => array(
@@ -32,20 +32,12 @@ class User extends AppModel {
         ),
         'user_role' => array(
             'valid' => array(
-                'rule' => array('inList', array('admin', 'user')),
+                'rule' => array('inList', array('admin', 'author')),
                 'message' => 'Please enter a valid role',
                 'allowEmpty' => false
             )
         )
     );
-	// ...
-
-public function beforeSave($options = array()) {
-    if (isset($this->data[$this->alias]['user_password'])) {
-        $this->data[$this->alias]['user_password'] = AuthComponent::password($this->data[$this->alias]['user_password']);
-    }
-    return true;
-}
 
 // ...
 /**
--- trunk/app/View/Users/login.ctp
+++ trunk/app/View/Users/login.ctp
@@ -2,7 +2,7 @@
 <?php echo $this->Session->flash('auth'); ?>
 <?php echo $this->Form->create('User'); ?>
     <fieldset>
-        <legend><?php echo __('Please enter your email address and password'); ?></legend>
+        <legend><?php echo __('Please enter your username and password'); ?></legend>
     <?php
         echo $this->Form->input('user_email');
         echo $this->Form->input('user_password');
