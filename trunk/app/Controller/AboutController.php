6bcbe2d65c452b81b72d0e88ce417494a015139d
sippoujulian@gmail.com <sippoujulian@gmail.com@b32a8489-b907-6ae1-0a5f-8c12d9beddb8>
2012-09-17 12:46:35 +0000

    AboutController.php and contact_us.ctp

---
+++ trunk/app/Controller/AboutController.php
@@ -0,0 +1,19 @@
+<?php
+
+App::uses('AppController', 'Controller');
+
+/**
+ * Tours Controller
+ *
+ * @property Tour $Tour
+ */
+class AboutController extends AppController {
+
+    public function aboutCompany() {
+    }
+    public function contactUs() {
+    }
+
+}
+
+?>
