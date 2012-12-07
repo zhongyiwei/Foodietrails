d7358f8aee3b70f03ec2fe1d29226baa3eecfa00
sippoujulian@gmail.com <sippoujulian@gmail.com@b32a8489-b907-6ae1-0a5f-8c12d9beddb8>
2012-12-07 13:05:18 +0000

    Fixed Paypal Bug again

--- trunk/app/webroot/confirmation.php
+++ trunk/app/webroot/confirmation.php
@@ -1,7 +1,9 @@
 <?php
-    setcookie("payer_id", $_GET["PayerID"]);
+$payerId = $_GET['PayerID'];
+setcookie("payer_id", $payerId);
 ?>
 Please wait for redirecting
 <script language="javascript" type="text/javascript">
-    window.setTimeout('window.location="http://localhost/confirmationLanding.php"; ',0);
+//    window.setTimeout('window.location="http://localhost/confirmationLanding.php"; ',0);
+    window.setTimeout('window.location="http://ie.infotech.monash.edu.au/project35/review4/confirmationLanding.php"; ',0);
 </script>
